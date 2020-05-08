d283dd6c4b1f3d4bcf16dc18a0bc04d59ff0ec5a
RAJANI KUMAR <rohit@RAJANIs-MacBook-Air.local>
2020-05-08 23:30:03 +0530

    pushed config changes

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,96 +1,145 @@
-#----networking/main.tf----
+# vpc.tf 
+# Create VPC/Subnet/Security Group/Network ACL
 provider "aws" {
   version = "~> 2.0"
   region     = var.region
 }
-
-data "aws_availability_zones" "available" {
+# create the VPC
+resource "aws_vpc" "My_VPC" {
+  cidr_block           = var.vpcCIDRblock
+  instance_tenancy     = var.instanceTenancy 
+  enable_dns_support   = var.dnsSupport 
+  enable_dns_hostnames = var.dnsHostNames
+tags = {
+    Name = "My VPC"
 }
-
-resource "aws_vpc" "tf_vpc" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "tf_vpc"
-  }
+} # end resource
+# create the Subnet
+resource "aws_subnet" "My_VPC_Subnet" {
+  vpc_id                  = aws_vpc.My_VPC.id
+  cidr_block              = var.subnetCIDRblock
+  map_public_ip_on_launch = var.mapPublicIP 
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "My VPC Subnet"
 }
-
-resource "aws_internet_gateway" "tf_internet_gateway" {
-  vpc_id = aws_vpc.tf_vpc.id
-
-  tags = {
-    Name = "tf_igw"
-  }
-}
-
-resource "aws_route_table" "tf_public_rt" {
-  vpc_id = aws_vpc.tf_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.tf_internet_gateway.id
-  }
-
-  tags = {
-    Name = "tf_public"
-  }
-}
-
-resource "aws_default_route_table" "tf_private_rt" {
-  default_route_table_id = aws_vpc.tf_vpc.default_route_table_id
-
-  tags = {
-    Name = "tf_private"
-  }
-}
-
-resource "aws_subnet" "tf_public_subnet" {
-  count                   = 2
-  vpc_id                  = aws_vpc.tf_vpc.id
-  cidr_block              = var.public_cidrs[count.index]
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "tf_public_${count.index + 1}"
-  }
-}
-
-resource "aws_route_table_association" "tf_public_assoc" {
-  count          = length(aws_subnet.tf_public_subnet)
-  subnet_id      = aws_subnet.tf_public_subnet[count.index].id
-  route_table_id = aws_route_table.tf_public_rt.id
-}
-
-resource "aws_security_group" "tf_public_sg" {
-  name        = "tf_public_sg"
-  description = "Used for access to the public instances"
-  vpc_id      = aws_vpc.tf_vpc.id
-
-  #SSH
-
+} # end resource
+# Create the Security Group
+resource "aws_security_group" "My_VPC_Security_Group" {
+  vpc_id       = aws_vpc.My_VPC.id
+  name         = "My VPC Security Group"
+  description  = "My VPC Security Group"
+  
+  # allow ingress of port 22
   ingress {
+    cidr_blocks = var.ingressCIDRblock  
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.accessip]
-  }
-
-  #HTTP
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [var.accessip]
-  }
-
+  } 
+  
+  # allow egress of all ports
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+tags = {
+   Name = "My VPC Security Group"
+   Description = "My VPC Security Group"
 }
+} # end resource
+# create VPC Network access control list
+resource "aws_network_acl" "My_VPC_Security_ACL" {
+  vpc_id = aws_vpc.My_VPC.id
+  subnet_ids = [ aws_subnet.My_VPC_Subnet.id ]
+# allow ingress port 22
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = var.destinationCIDRblock 
+    from_port  = 22
+    to_port    = 22
+  }
+  
+  # allow ingress port 80 
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = var.destinationCIDRblock 
+    from_port  = 80
+    to_port    = 80
+  }
+  
+  # allow ingress ephemeral ports 
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = var.destinationCIDRblock
+    from_port  = 1024
+    to_port    = 65535
+  }
+  
+  # allow egress port 22 
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = var.destinationCIDRblock
+    from_port  = 22 
+    to_port    = 22
+  }
+  
+  # allow egress port 80 
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = var.destinationCIDRblock
+    from_port  = 80  
+    to_port    = 80 
+  }
+ 
+  # allow egress ephemeral ports
+  egress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = var.destinationCIDRblock
+    from_port  = 1024
+    to_port    = 65535
+  }
+tags = {
+    Name = "My VPC ACL"
+}
+} # end resource
+# Create the Internet Gateway
+resource "aws_internet_gateway" "My_VPC_GW" {
+ vpc_id = aws_vpc.My_VPC.id
+ tags = {
+        Name = "My VPC Internet Gateway"
+}
+} # end resource
+# Create the Route Table
+resource "aws_route_table" "My_VPC_route_table" {
+ vpc_id = aws_vpc.My_VPC.id
+ tags = {
+        Name = "My VPC Route Table"
+}
+} # end resource
+# Create the Internet Access
+resource "aws_route" "My_VPC_internet_access" {
+  route_table_id         = aws_route_table.My_VPC_route_table.id
+  destination_cidr_block = var.destinationCIDRblock
+  gateway_id             = aws_internet_gateway.My_VPC_GW.id
+} # end resource
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "My_VPC_association" {
+  subnet_id      = aws_subnet.My_VPC_Subnet.id
+  route_table_id = aws_route_table.My_VPC_route_table.id
+} # end resource
+# end vpc.tf
